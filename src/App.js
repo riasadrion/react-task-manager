7a152b43d7f264f2dad4378a79ab15c8440f84bd
Riasad Rion <riasadrion@gmail.com>
2021-08-13 22:26:57 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-
+import Navbar from './components/Navbar'
 function App() {
   return (
     <div className="App">
-      <h1>Hellxo React!</h1>
+      <Navbar />
     </div>
   );
 }
