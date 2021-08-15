fa804efa362403ff4cff796d5cc7e38929afcf35
Riasad Rion <riasadrion@gmail.com>
2021-08-15 23:18:33 +0600

    nav component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 function Navbar() {
     return (
         <div>
-            <nav></nav>
+            <nav className="flex-1 caret-yellow-100">
+                <div className="flex-2">Brand</div>
+                <div className="flex-2">Brand</div>
+                <div className="flex-2">Brand</div>
+            </nav>
         </div>
     )
 }
